b284cf3a07fa4b9d761b1030d84343fe1d6c9475
NicoDalmas <ndalmas9@gmail.com>
2016-11-18 06:28:50 -0300

    Casa 17/11/2016

--- editar.php
+++ editar.php
@@ -19,16 +19,80 @@ if (isset($_GET['id'])) {
 <!DOCTYPE html>
 <html>
 <head>
+
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="http://getbootstrap.com/docs-assets/ico/favicon.png">
+
+    
+    <!-- Bootstrap core CSS -->
+    <link href="Sticky%20Footer%20Template%20for%20Bootstrap_files/bootstrap.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="Sticky%20Footer%20Template%20for%20Bootstrap_files/sticky-footer.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script src="../../docs-assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
 	<meta charset="utf-8">
 	<title>Editar clientes</title>
+	<link rel="stylesheet" type="text/css" href="bootstrap.css">
 </head>
 <body>
-	<h2>Editar cliente</h2>
-	<form action="crud.php" method="post">
+<div id="wrap">
+<div class="container">
+
+<div class="page-header">
+          
+       
+	<h2>EditarCliente <small>Niquillo</small></h2>
+</div>	
+	<form action="crud.php" method="post" class="form-group">
+		<div class="form-group">
 		<p>Nombre: <input type="text" name="nombre" value="<?php echo $datos['nombre'] ?>"></p>
 		<p>Domicilio: <input type="text" name="domicilio" value="<?php echo $datos['domicilio'] ?>"></p>
 		<input type="hidden" name="id" value="<?php echo $id; ?>">
-		<input type="submit" name="editar" value="Guardar">
+		<input type="submit" name="editar" value="Guardar" class="btn btn-primary">
+		</div>
 	</form>
+	  
+      
+      
+
+
+       
+        
+      </div>
+    </div>
+
+    <div id="footer">
+      <div class="container">
+
+
+        <p class="text-muted credit">Example courtesy <a href="http://martinbean.co.uk/">Martin Bean</a> and <a href="http://ryanfait.com/sticky-footer/">Ryan Fait</a>.</p>
+
+
+
+      </div>
+    </div>
+	<!--
+	<div id="footer">
+      <div class="container">
+
+        <p class="text-muted credit">Practica PHP 2016.</p>
+
+      </div>
+    </div>
+    -->
+	
 </body>
 </html>
